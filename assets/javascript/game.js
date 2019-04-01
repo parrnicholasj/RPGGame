025497d826a0cac6854bdd758b50705c81d2a773
parrn1 <mrnickp@comcast.net>
2019-04-01 13:49:59 -0400

    set up base html and javascript

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,9 +1,82 @@
-//stage character select
-// selected characters large in center of screen for player to choose
 
-//stage 2
-//player char is large and on one side of screen with remaining enemies highlighted while defeated enemies are greyed out
+$game(document).ready(function(){
+  
+  //define character objects
+  
+  var Dorn = {
+  
+  health: 150,
+  attack: 10,//this updates after each attack by player
+  attackPower: 10,//this never changes
+  isPlayer: false,
+  active: false
+  
+  }
+  
+  var Perturabo = {
+  
+  health: 100,
+  attack: 5,
+  attackPower: 5,
+  isPlayer: false,
+  active: false
+  
+  }
+  
+  var Angron = {
+  
+  health: 125,
+  attack: 15,
+  attackPower: 15,
+  isPlayer: false,
+  active: false
+  
+  }
+  
+  var sanguinius = {
+  
+  health: 200,
+  attack: 20,
+  attackPower: 20,
+  isPlayer: false,
+  active: false
+  
+  }
+  
+  var charSelectStage = true;//starts at char select
+  var pickOpponentStage = false;
+  var fightStage = false;
 
-//stage 3
-//fight stage player and enemy they are fighting are large and on either side of screen with other enemies at top
-// player fights until win or death
+  //stage character select
+  // selected characters large in center of screen for player to choose
+  
+if(charSelectStage)
+{
+
+//set to col-3s reduce margin on container
+
+}
+
+  //stage 2
+  //player char is large and on one side of screen with remaining enemies highlighted while defeated enemies are greyed out
+  
+if(pickOpponentStage)
+{
+
+  // make player char col-6 others col-4
+
+}
+
+  //stage 3
+  //fight stage player and enemy they are fighting are large and on either side of screen with other enemies at top
+  // player fights until win or death
+
+if (fightStage)
+{
+
+// two active ones are col-6 others hidden
+
+}
+
+
+})
