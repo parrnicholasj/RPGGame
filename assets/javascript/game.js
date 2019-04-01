93a52c3868e1dd8fd59b1518f81e51c68ff8fc46
Nicholas Parr <parrnicholasj@gmail.com>
2019-03-31 23:58:31 -0400

    psuedo code

---
+++ assets/javascript/game.js
@@ -0,0 +1,9 @@
+//stage character select
+// selected characters large in center of screen for player to choose
+
+//stage 2
+//player char is large and on one side of screen with remaining enemies highlighted while defeated enemies are greyed out
+
+//stage 3
+//fight stage player and enemy they are fighting are large and on either side of screen with other enemies at top
+// player fights until win or death
