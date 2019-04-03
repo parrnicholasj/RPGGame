162a1965c8568223562ab401beea36f3e65e9c8b
Nicholas Parr <parrnicholasj@gmail.com>
2019-04-02 22:14:34 -0400

    game complete

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,9 +5,9 @@ $(document).ready(function () {
     Dorn = {
 
       name: "Dorn",
-      health: 150,
-      attack: 10, //this updates after each attack by player
-      attackPower: 10, //this never changes
+      health: 275,
+      attack: 15, //this updates after each attack by player
+      attackPower: 15, //this never changes
       isPlayer: false,
       active: false
 
@@ -16,9 +16,9 @@ $(document).ready(function () {
     Perturabo = {
 
       name: "Perturabo",
-      health: 100,
-      attack: 5,
-      attackPower: 5,
+      health: 140,
+      attack: 35,
+      attackPower: 35,
       isPlayer: false,
       active: false
 
@@ -27,9 +27,9 @@ $(document).ready(function () {
     Angron = {
 
       name: "Angron",
-      health: 125,
-      attack: 15,
-      attackPower: 15,
+      health: 145,
+      attack: 35,
+      attackPower: 35,
       isPlayer: false,
       active: false
 
@@ -51,7 +51,7 @@ $(document).ready(function () {
 
     `<div id="Dorn" class="card text-center">
 <div class="card-header">
-  Dorn
+<h2>Dorn</h2>
 </div>
 <div class="card-body">
   <img class="card-img-top" src="assets/images/dorn.jpg" alt="Rogal Dorn">
@@ -59,11 +59,11 @@ $(document).ready(function () {
 <div class="card-footer text-muted">
   2 days ago
 </div>
-</div>`,//perturabo -----------------------------------------------------------------------------------------
+</div>`, //perturabo -----------------------------------------------------------------------------------------
 
-`<div id="Perturabo" class="card text-center">
+    `<div id="Perturabo" class="card text-center">
 <div class="card-header">
-  Perturabo
+<h2>Perturabo</h2>
 </div>
 <div class="card-body">
   <img class="card-img-top" src="assets/images/perturabo.jpg" alt="Perturabo">
@@ -71,11 +71,11 @@ $(document).ready(function () {
 <div class="card-footer text-muted">
   2 days ago
 </div>
-</div>`,//Angron ---------------------------------------------------------------------------------------------
+</div>`, //Angron ---------------------------------------------------------------------------------------------
 
-`<div id="Angron" class="card text-center">
+    `<div id="Angron" class="card text-center">
 <div class="card-header">
-  Angron
+<h2>Angron</h2>
 </div>
 <div class="card-body">
   <img class="card-img-top" src="assets/images/Angron.jpg" alt="Angron">
@@ -83,11 +83,11 @@ $(document).ready(function () {
 <div class="card-footer text-muted">
   2 days ago
 </div>
-</div>`,// Sanguinius _---------------------------------------------------------------------------------------
+</div>`, // Sanguinius _---------------------------------------------------------------------------------------
 
-`<div id="Sanguinius" class="card text-center">
+    `<div id="Sanguinius" class="card text-center">
 <div class="card-header">
-  Sanguinius
+<h2>Sanguinius</h2>
 </div>
 <div class="card-body">
   <img class="card-img-top" src="assets/images/sanguinius.jpg" alt="Sanguinius">
@@ -106,15 +106,34 @@ $(document).ready(function () {
   var charSelectStage = true; //starts at char select
   var pickOpponentStage = false;
   var fightStage = false;
+  var wins = 0;
 
   var a;
-  
+  var b;
+
   //function builds out the new order so they can be styled correctly
 
-  function builder (x){//give index of temps slot to put in order
+  function builder(x) { //give index of temps slot to put in order
 
-$(".row").append(temps[x]);
+    $(".row").append(temps[x]);
 
+
+  }
+
+  //apply health updates to characters
+  function updateHealth(){
+
+    for(var i = 0; i < primarchs.length; i++){
+
+      $("#" + primarchs[i].name).find(".card-footer").html(`<h3>Health: ${primarchs[i].health}`)
+
+    }
+
+  }
+
+  function updateInstruct(x){
+
+    $("#instruct").html(`<h2>${x}</h2>`)
 
   }
 
@@ -123,57 +142,63 @@ $(".row").append(temps[x]);
 
   if (charSelectStage) {
 
+    updateHealth();
+    updateInstruct("Choose your character");
+
     //set to col-3s reduce margin on container
     $(".card").removeClass("col-6 col-4").addClass("col-3");
 
     $("#characters").on("click", ".card", function () { //can only click on things that were always there from beginnig
 
+      if (charSelectStage) {
+        for (var i = 0; i < primarchs.length; i++) {
 
-      for (var i = 0; i < primarchs.length; i++) {
+          if (($(this).attr('id') == primarchs[i].name) && charSelectStage) {
 
-        if (($(this).attr('id') == primarchs[i].name) && charSelectStage) {
-
-          primarchs[i].isPlayer = true;
-          console.log(primarchs[i]);
-          charSelectStage = false;
-          pickOpponentStage = true;
-          a = i;
-          
-
-        }
-
-      }
-
-      //remove everything but selected to rebuild in order
-      $(".card").not($(this)).remove();
-
-      //get first index then build the rest
-
-      
-      for (var i = 0; i < temps.length; i++){
-
-        if (i != a){
-          builder(i);
-          console.log(i);
-        }
-
-      }
-
-      $(".card").removeClass("col-9 col-3").addClass("col-4 enemy"); //set everyone then overwrite for player
-
-      for (var i = 0; i < primarchs.length; i++) { //move everything to stage two positions
-
-        console.log(primarchs[i].isPlayer);
-        if (($(this).attr('id') == primarchs[i].name) && primarchs[i].isPlayer == true) { //find player and make them take up col-6
+            primarchs[i].isPlayer = true;
+            console.log(primarchs[i]);
+            charSelectStage = false;
+            pickOpponentStage = true;
+            a = i;
 
 
-          console.log(primarchs[i]);
-          $(this).removeClass("col-3 col-4").addClass("col-9 player");
+          }
 
         }
 
+        //remove everything but selected to rebuild in order
+        $(".card").not($(this)).remove();
+
+        //get first index then build the rest
 
 
+        for (var i = 0; i < temps.length; i++) {
+
+          if (i != a) {
+            builder(i);
+            console.log(i);
+          }
+
+        }
+
+        $(".card").removeClass("col-6 col-3").addClass("col-4 enemy"); //set everyone then overwrite for player
+
+        for (var i = 0; i < primarchs.length; i++) { //move everything to stage two positions
+
+          console.log(primarchs[i].isPlayer);
+          if (($(this).attr('id') == primarchs[i].name) && primarchs[i].isPlayer == true) { //find player and make them take up col-6
+
+
+            console.log(primarchs[i]);
+            $(this).removeClass("col-3 col-4 enemy").addClass("col-9 player");
+
+          }
+
+
+
+        }
+        updateHealth();
+        updateInstruct("Choose an opponent");
       }
 
     });
@@ -187,24 +212,48 @@ $(".row").append(temps[x]);
 
   $("#characters").on("click", ".enemy", function () {
 
-    console.log("click");
+    console.log(this);
+    var notdead = false;//need to check if alive before proceeding
+    for (var i = 0; i < primarchs.length; i++) {
+      if (($(this).attr('id') == primarchs[i].name) && (primarchs[i].health > 0) ){
+        notdead = true;
+      }
+    }
 
-    if (pickOpponentStage){
-      
+
+    if (pickOpponentStage && notdead) {
+
 
       for (var i = 0; i < primarchs.length; i++) {
 
-        if (($(this).attr('id') == primarchs[i].name) && pickOpponentStage) {
+        if (($(this).attr('id') == primarchs[i].name) && pickOpponentStage && (primarchs[i].health > 0)) {
 
+          
           primarchs[i].active = true;
           console.log(primarchs[i]);
           pickOpponentStage = false
           fightStage = true;
-          $(this).removeClass("col-3 col-4").addClass("col-9");          
+          $(this).removeClass("col-3 col-4").addClass("col-6 active");
+          $(".player").removeClass("col-3 col-9").addClass("col-6");
+          b = i;
 
         }
 
       }
+      console.log(b);
+      console.log(a);
+      $(".card").not($(this,)).not($(".player")).remove();//clean out others so we can rebuild
+
+      for (var i = 0; i < temps.length; i++) {
+
+        if ((i != a) && (i != b)) {
+          builder(i);
+          $("#" + primarchs[i].name).addClass("col-3 enemy");
+        }
+
+      }
+      updateHealth();
+      updateInstruct("Click on opponent to attack");
 
     }
 
@@ -214,11 +263,48 @@ $(".row").append(temps[x]);
   //fight stage player and enemy they are fighting are large and on either side of screen with other enemies at top
   // player fights until win or death
 
-  if (fightStage) {
+  $("#characters").on("click", ".active", function () {
 
-    // two active ones are col-6 others hidden
+    //deal damage and increase player attack after each click then update health
 
-  }
+    if (fightStage)
+    {
+
+      primarchs[b].health -= primarchs[a].attack;
+      primarchs[a].health -= primarchs[b].attack;
+      primarchs[a].attack += primarchs[a].attackPower;//increase players attack power
+      updateHealth();
+
+      //when enemy defeated set up to select next enemy
+      if (primarchs[b].health <= 0){
+
+        $(".enemy").removeClass("col-6 col-3 active").addClass("col-4 enemy"); 
+        $(".player").removeClass("col-3 col-4").addClass("col-9");
+
+       
+        updateHealth();
+        updateInstruct("Choose next opponent");
+
+        fightStage = false;
+        pickOpponentStage = true;
+        primarchs[b].active = false;
+        wins++;
+
+        if (wins >= 3){alert("you are the victor!")}
+
+      }
+
+      if (primarchs[a].health <= 0){
+
+        alert("You have been defeated");
+
+      }
+
+
+
+    }
+
+  })
 
 
 })
